24d391d56027ef2f504b9dce36d109d4a555c595
Ammar Lakis <ammar.lakis@gmail.com>
2025-04-17 23:42:22 +0200

    WIP

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import copy from 'rollup-plugin-copy';
 
 export default defineConfig({
   site: 'https://ammarlakis.github.io/security-awareness-arabic/',
+  base: '/security-awareness-arabic/',
   integrations: [react()],
   markdown: {
     syntaxHighlight: 'prism',
