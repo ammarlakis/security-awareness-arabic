f717bb9b66609c3117a4fff553e1a50529fe5b60
Ammar Lakis <ammar.lakis@gmail.com>
2025-04-17 15:54:07 +0200

    WIP

---
+++ src/utils/courseIndex.js
@@ -0,0 +1,13 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+export default async function courseIndex() {
+  const courseDir = './src/courses';
+  const folders = await fs.readdir(courseDir);
+  const result = await Promise.all(folders.map(async folder => {
+    const metaPath = path.join(courseDir, folder, 'meta.json');
+    const meta = JSON.parse(await fs.readFile(metaPath, 'utf-8'));
+    return { title: meta.title, url: `/courses/${folder}/lesson1` };
+  }));
+  return result;
+}
