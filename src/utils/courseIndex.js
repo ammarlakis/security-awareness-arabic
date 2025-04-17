c0b1cc3c3e81fca3530afed4270f74f5cbc3652b
Ammar Lakis <ammar.lakis@gmail.com>
2025-04-17 23:51:51 +0200

    WIP

--- src/utils/courseIndex.js
+++ src/utils/courseIndex.js
@@ -1,5 +1,6 @@
 import fs from 'fs/promises';
 import path from 'path';
+import { Astro } from 'astro';
 
 export default async function courseIndex() {
   const courseDir = './src/courses';
@@ -7,7 +8,7 @@ export default async function courseIndex() {
   const result = await Promise.all(folders.map(async folder => {
     const metaPath = path.join(courseDir, folder, 'meta.json');
     const meta = JSON.parse(await fs.readFile(metaPath, 'utf-8'));
-    return { title: meta.title, url: `/courses/${folder}/lesson1`, image: meta.image };
+    return { title: meta.title, url: `${Astro.site}/courses/${folder}/lesson1`, image: meta.image };
   }));
   return result;
 }
